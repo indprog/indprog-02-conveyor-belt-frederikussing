2122adcb054f3102da6d018a87096337af5cd7d4
S245857 <s245857@student.dtu.dk>
2026-09-14 15:11:05 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-
-motors = int(input()) 
-weight = int(input())
-
-if weight / motors <= 12:
-    print("Yes! The conveyor belt can carry the packages.")
-else:
-    print("No. The conveyor belt cannot carry the packages.")
-
